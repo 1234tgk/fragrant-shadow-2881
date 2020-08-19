a296dc701289a7ab2170dccc150da34734a48950
Taehyeon Kim <1234tgk@gmail.com>
2020-08-19 04:10:51 -0400

    Fixed Help Command

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,6 +1,6 @@
 async def help_me(message):
-    help_str = "Here are the list of commands:" \
-               "!help: See the list of commands" \
-               "!hi: Bot'Zach will greet you back" \
-               "!time: Display the current time in UTC"
+    help_str = "Here are the list of commands:\n" \
+               "!help: See the list of commands\n" \
+               "!hi: Bot'Zach will greet you back\n" \
+               "!time: Display the current time in UTC\n"
     await message.channel.send(help_str)
